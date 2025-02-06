daa498316f4ffade87dfcb87cd4a0ddada56d13d
arbaz khan <158210343+arbazkhan97@users.noreply.github.com>
2025-02-06 10:52:55 +0000

    add extension

--- basics/first.js
+++ basics/first.js
@@ -1,16 +1,1 @@
-console.log('arbaz khan how are you');
 
-for(let i=0;i<10;i++){
-console.log(i);
-}
-
-for(let j=10;j>=0;j--){
-console.log(j);
-}
-
-for(let i=1;i<=2;i++){
-console.log(i);
-}
-
-let arr=[1,2,3,4,5,6,7,8,9,10];
-console.table(arr);
