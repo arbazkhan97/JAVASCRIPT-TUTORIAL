af83d0345d10dccb41897ffccfe27fee225237ad
arbaz khan <158210343+arbazkhan97@users.noreply.github.com>
2025-02-07 12:09:45 +0000

    learn about number and Math liibrary

---
+++ basics/num_maths.js
@@ -0,0 +1,11 @@
+
+let piValue=Math.PI;
+// console.log(piValue);
+
+let val=567
+// console.log(Math.sqrt(val))
+
+let min=20;
+let max=30;
+let number=Math.floor(Math.random()*(max-min+1)+min)
+console.log(Math.floor(Math.random()*10+1));
