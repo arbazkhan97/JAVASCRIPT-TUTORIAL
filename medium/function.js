b376bfa31719a85df222f18560d26d1bbdab0a76
arbaz khan <158210343+arbazkhan97@users.noreply.github.com>
2025-02-12 08:37:41 +0000

    Learn arrow() This keyword  implicit return and explicit return

--- medium/function.js
+++ medium/function.js
@@ -5,7 +5,7 @@ function is a re-usable block of code
 
 function parameter=>  when we declared a function and passs the variable that is called function parameter
 
-fucntion argument=> during function we pass the values that is called function argument 
+fucntion argument=> during function call when we pass the variable or values that is called function argument 
 
 return()=>  it returns the  function values
 
@@ -79,4 +79,115 @@ function addToCards(...items){
     return ` ${getMYArray[0]}, ${getMYArray[2]} `
   }
 
-  console.log(myArrayFunction(array1))
+//   console.log(myArrayFunction(array1))
+
+
+// ********* This Keyword ************************
+
+/*
+This keyword refers to current context
+
+if we print the (This) keyword in node js enviornment then it  returns the empty object ({}) .
+
+if we print the (This) keyword in  brower then it return the  window object
+
+let's try some example 
+
+*/
+
+const thisUserObj={
+    name:'arbaz khan',
+    age:23,
+    welcomeMsg:function(){
+        // console.log(this.name,this.age);
+        // console.log(this)
+    }
+}
+thisUserObj.name='salman khan'
+// console.log(this)
+ thisUserObj.welcomeMsg()
+
+ // this keyword with functions
+
+ function one(){
+    // const username='arbaz khan'
+  console.log(this)
+ }
+ 
+//  one()
+/* 
+when we print the (This) keyword inside the normal function then it return  an object which will be unreadable form
+*/
+
+const two=function(){
+    // const username='salman khan'
+    console.log(this)
+}
+// two()
+
+const three=()=>{
+
+    
+    console.log(this)
+}
+
+// three()
+
+/*
+
+when we print the (this) keyword  inside the arrow function then it returns the  empty object ( {} )
+*/
+
+
+/* 
+
+implicit return means  no need to add {}  in function 
+      ****example******
+  */
+
+
+      const addArrow=(num1,num2)=>  (num1+num2)
+
+
+    //   console.log(addArrow(2,3))
+
+      // secong
+
+      const some=()=>{ return ({username:'arbaz khan',age:23,course:'BCA'})}
+
+    //   console.log(some())
+      /* 
+
+explicit return means  we have to  add  {}  in function  (compulsary) 
+
+      ****example******
+  */
+
+      const multiple=(num,num2)=>{
+        return num**num2
+      }
+
+    //   console.log(multiple(2,20))
+
+
+    function chai(user){
+
+        if(user){
+            return `${user} just logged in`
+        }
+        else{
+            console.log('please enter username')
+            return
+        }
+    }
+//    console.log( chai('arbaz khan'))
+
+
+const spread=(...items)=>{
+
+    const mul=items.map((i)=>i+(i*18/100))
+    return mul;
+}
+
+ const values=spread(12,12,13,13,14,14,15,15)
+//  console.log('18% gst included',values)-
